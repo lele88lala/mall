9a7604be4d81e29840b65528f54aba11b7c70d17
fenghuan <1207018406@qq.com>
2016-05-03 22:46:56 +0800

    add rule to mall detail

--- js/malldetail.js
+++ js/malldetail.js
@@ -9,7 +9,7 @@ document.querySelector(".wrap").addEventListener("touchmove", touchHandler, fals
 document.querySelector(".wrap").addEventListener("touchend", touchHandler, false);
 var xDown = null;
 var slideWidth = document.querySelector(".holder").offsetWidth;
-var index = 0;
+var ind= 0;
 var xDiff = null;
 var arr = new Array("dot-pic1", "dot-pic2", "dot-pic3");
 var len = document.querySelector("html").offsetWidth / 20;
@@ -17,25 +17,25 @@ function touchHandler(e) {
     if (e.type == "touchstart") {
         xDown = e.touches[0].clientX;
         document.querySelector(".animate").removeAttribute("animate");
-        document.querySelector("#" + arr[index] + "").setAttribute("class", "docselectcolor");
+        document.querySelector("#" + arr[ind] + "").setAttribute("class", "docselectcolor");
         //document.querySelector("#"+arr[index]+"").classList.remove("dotcolor");
     } else if (e.type == "touchmove") {
         var xUp = e.touches[0].clientX;
-        xDiff = index * slideWidth + xDown - xUp;
+        xDiff = ind * slideWidth + xDown - xUp;
         if (xDiff < 800) {
             document.querySelector(".wrap").style.cssText = "transform:translate3d(-" + xDiff + "px,0,0)";
         }
     } else if (e.type == "touchend" || e.type == "touchcancel") {
-        var absMove = Math.abs(index * slideWidth - xDiff);
+        var absMove = Math.abs(ind * slideWidth - xDiff);
         if (absMove > slideWidth / 5) {
-            if (xDiff > index * slideWidth && index < 2) {
-                index++;
-            } else if (xDiff < index * slideWidth && index > 0) {
-                index--;
+            if (xDiff > ind * slideWidth && ind < 2) {
+                ind++;
+            } else if (xDiff < ind * slideWidth && ind > 0) {
+                ind--;
             }
         }
-        document.querySelector("#" + arr[index] + "").setAttribute("class", "dotcolor");
-        document.querySelector(".animate").style.cssText = "transform:translate3d(-" + index * slideWidth + "px,0,0)";
+        document.querySelector("#" + arr[ind] + "").setAttribute("class", "dotcolor");
+        document.querySelector(".animate").style.cssText = "transform:translate3d(-" + ind * slideWidth + "px,0,0)";
     }
 }
 //buy count
@@ -60,7 +60,7 @@ for (var index = 0 ;index < rules.length; index++) {
         if (this.querySelector(".arrow").classList.contains("arrow-down")) {
             this.querySelector(".arrow").classList.remove("arrow-down");
             this.querySelector(".arrow").classList.add("arrow-up");
-            this.querySelector(".rule-detail").style.height = "10rem";
+            this.querySelector(".rule-detail").style.height = "12rem";
             this.querySelector(".rule-detail").classList.add("rule-detail-border");
         } else {
             this.querySelector(".arrow").classList.remove("arrow-up");
@@ -68,7 +68,5 @@ for (var index = 0 ;index < rules.length; index++) {
             this.querySelector(".rule-detail").style.height = "0rem";
             this.querySelector(".rule-detail").classList.remove("rule-detail-border");
         }
-
-
     }
 }
